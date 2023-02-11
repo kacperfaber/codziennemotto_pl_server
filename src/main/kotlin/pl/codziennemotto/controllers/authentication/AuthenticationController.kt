1bb9617da3295ada75e8e9824859ed240b458983
Kacper Faber <kacperf1234@gmail.com>
2023-02-11 15:54:08 +0100

    Created AuthenticationController with simple '/auth' POST endpoint

---
+++ src/main/kotlin/pl/codziennemotto/controllers/authentication/AuthenticationController.kt
@@ -0,0 +1,31 @@
+package pl.codziennemotto.controllers.authentication
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+import pl.codziennemotto.controllers.ControllerBase
+import pl.codziennemotto.services.authentication.AuthenticationService
+import pl.codziennemotto.services.token.TokenService
+
+@RestController
+class AuthenticationController(private val tokenService: TokenService, private val authenticationService: AuthenticationService) :
+    ControllerBase() {
+
+    class AuthPayload {
+        lateinit var login: String
+        lateinit var password: String
+    }
+
+    data class AuthResponse(val userId: Int, val userEmail: String, val username: String, val token: String)
+
+    @PostMapping("/auth")
+    fun authEndpoint(@RequestBody authPayload: AuthPayload): ResponseEntity<AuthResponse> {
+        val authenticatedUser = authenticationService.tryAuthenticate(authPayload.login, authPayload.password)
+            ?: return badRequest()
+
+        val token = tokenService.generateToken(authenticatedUser)
+
+        return ok(AuthResponse(authenticatedUser.id!!, authenticatedUser.email, authenticatedUser.username, token))
+    }
+}
