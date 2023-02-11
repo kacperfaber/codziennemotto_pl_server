3a0dc031425eaff9fa272e05d1a326c461e5bc42
Kacper Faber <kacperf1234@gmail.com>
2023-02-11 15:40:24 +0100

    Deleted application.properties, in Main.kt added profile dependent file [in schema 'application.{profile}.properties'

--- src/main/kotlin/pl/codziennemotto/Main.kt
+++ src/main/kotlin/pl/codziennemotto/Main.kt
@@ -2,8 +2,10 @@ package pl.codziennemotto
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.PropertySource
 
 @SpringBootApplication
+@PropertySource("application.\${spring.profiles.active}.properties")
 class Main
 
 fun main(args: Array<String>) {
