de7b57b51176ea5779129185da32ac7eba84db3c
Kacper Faber <kacperf1234@gmail.com>
2023-02-11 16:10:10 +0100

    Created simple GsonConfiguration.

---
+++ src/main/kotlin/pl/codziennemotto/GsonConfiguration.kt
@@ -0,0 +1,12 @@
+package pl.codziennemotto
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class GsonConfiguration {
+    @Bean
+    fun gsonBean(): Gson = GsonBuilder().create()
+}
