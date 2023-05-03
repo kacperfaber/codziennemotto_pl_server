9270e81952b1045d8061ec625fa3e4a36c58d333
Kacper Faber <kacperf1234@gmail.com>
2023-05-03 14:24:52 +0200

    Added CORS headers.

--- src/main/kotlin/pl/codziennemotto/Main.kt
+++ src/main/kotlin/pl/codziennemotto/Main.kt
@@ -2,9 +2,11 @@ package pl.codziennemotto
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.ServletComponentScan
 import org.springframework.context.annotation.PropertySource
 
 @SpringBootApplication
+@ServletComponentScan
 @PropertySource("classpath:application.\${spring.profiles.active}.properties")
 class Main
 
---
+++ src/main/kotlin/pl/codziennemotto/security/CorsFilter.kt
@@ -0,0 +1,17 @@
+package pl.codziennemotto.security
+
+import jakarta.servlet.Filter
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletRequest
+import jakarta.servlet.ServletResponse
+import jakarta.servlet.annotation.WebFilter
+import jakarta.servlet.http.HttpServletResponse
+
+@WebFilter("*")
+class CorsFilter : Filter {
+    override fun doFilter(request: ServletRequest?, response: ServletResponse?, chain: FilterChain?) {
+        (response as? HttpServletResponse)?.addHeader("Access-Control-Allow-Origin", "*")
+        (response as? HttpServletResponse)?.addHeader("Access-Control-Allow-Headers", "*")
+        chain?.doFilter(request, response)
+    }
+}
