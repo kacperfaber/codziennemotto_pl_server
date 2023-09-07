df2f05bec86d77cd886bd64f663fc58c3d474026
Kacper Faber <kacperf1234@gmail.com>
2023-09-07 18:29:20 +0200

    CORS
    Added Allow-Origin header to config.

--- src/main/kotlin/pl/codziennemotto/security/CorsFilter.kt
+++ src/main/kotlin/pl/codziennemotto/security/CorsFilter.kt
@@ -6,11 +6,15 @@ import jakarta.servlet.ServletRequest
 import jakarta.servlet.ServletResponse
 import jakarta.servlet.annotation.WebFilter
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.beans.factory.annotation.Value
 
 @WebFilter("*")
 class CorsFilter : Filter {
+    @Value("\${cors.allow-origin}")
+    lateinit var allowOrigin: String
+
     override fun doFilter(request: ServletRequest?, response: ServletResponse?, chain: FilterChain?) {
-        (response as? HttpServletResponse)?.addHeader("Access-Control-Allow-Origin", "*")
+        (response as? HttpServletResponse)?.addHeader("Access-Control-Allow-Origin", allowOrigin)
         (response as? HttpServletResponse)?.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS")
         (response as? HttpServletResponse)?.addHeader("Access-Control-Allow-Headers", "*");
         chain?.doFilter(request, response)
